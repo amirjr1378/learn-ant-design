20c1d469d03edaf5ef4e8fa462f70c2a898e471a
Mohammad <developer.net@live.com>
2020-02-01 16:25:40 +0330

    change default vars

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { ConfigProvider } from "antd";
+import faIR from "antd/es/locale-provider/fa_IR";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <ConfigProvider>
+    <App />
+  </ConfigProvider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
